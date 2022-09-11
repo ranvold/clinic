e2a7d95ab7ee9c53d70a504c01e505f6489e4667
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-09-11 22:27:37 +0300

    add model Category and foreign key in Doctors

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_11_170316) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_11_192325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_170316) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
     t.index ["email"], name: "index_doctors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
   end
@@ -38,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_170316) do
     t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "doctors", "categories"
 end
