5a8bf0d8e32f895a0da035043c67daa9ecc7b8a9
Volodymyr Shafran <shafran.valdemarovich@gmail.com>
2022-09-12 10:42:51 +0300

    add field in form registraions for patients, some setup devise models

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,7 @@
 class Patient < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
